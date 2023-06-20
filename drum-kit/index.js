b62a7803be70ae1e4f832ad6c2fe4af8dd1da483
jeffwebd <125322542+jeffwebd@users.noreply.github.com>
2023-06-20 00:08:14 -0400

    drum-kit.add_keydown.1

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -2,12 +2,13 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
     drummerBoy(this.innerHTML)
 }
-)
+);
+document.addEventListener("keydown", function(event) {
+    drummerBoy(event.key);
+});
 };
 
-document.addEventListener("keydown", function(event)){
-    drummerBoy(event.key);
-}
+
 
 function drummerBoy(keykey){
     switch (keykey) {
