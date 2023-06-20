a6b7673bba67bb303b7b19af60af0c3ed2702d26
jeffwebd <125322542+jeffwebd@users.noreply.github.com>
2023-06-19 21:04:25 -0400

    drum-kit.addclicklistener.1.3

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,4 +1,4 @@
-for (var i = 0, i < document.querySelectorAll(".drum").length , i++) {
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
     alert("I got clicked")
 }
