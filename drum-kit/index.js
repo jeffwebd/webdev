25a9b3363ca4e26b59ad2fc67304c7661a09d87d
jeffwebd <125322542+jeffwebd@users.noreply.github.com>
2023-06-19 23:10:46 -0400

    drum-kit.add_sounds.0

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,6 +1,36 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    var audio = new Audio("./sounds/crash.mp3");
-    audio.play();
+
+    switch (this.innerHtml) {
+        case "w":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("./sounds/tom1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("./sounds/tom2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("./sounds/tom3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("./sounds/tom4.mp3");
+            tom4.play();
+            break;
+        default: 
+    }
 }
 )};
